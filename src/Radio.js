7497ddd1190417c102125e5db0aaa76ff365b364
jorgev259 <jorgev259@outlook.es>
2020-01-04 04:12:13 -0600

    mute/play/plause reowrk

--- src/Radio.js
+++ src/Radio.js
@@ -1,8 +1,9 @@
-/* global MediaMetadata */
+/* global MediaMetadata, Audio */
 import React from 'react'
 import $ from 'jquery'
 import io from 'socket.io-client'
 import Snowfall from 'react-snowfall'
+import { MdPlayArrow, MdPause, MdVolumeOff } from 'react-icons/md'
 
 import './css/glitch.css'
 import './css/player.css'
@@ -49,123 +50,36 @@ export default class Radio extends React.Component {
       album: 'Press the Play button to start the radio',
       title: placeholders[Math.floor(Math.random() * placeholders.length)]
     },
-    started: false
+    started: false,
+    playing: false,
+    muted: false
   }
 
   updateStation = (station, cb) => {
     this.socket.close()
+    this.audio.pause()
+
     this.setState({ station: station }, () => this.startSocket(cb))
   }
 
   startSocket = (cb = null) => {
     this.socket = io('https://api.squid-radio.net')
     this.socket.on(this.state.station, this.handleSong)
-    if (cb) cb()
-  }
 
-  handleSong = data => {
-    console.log(data)
-    if (data !== null) {
-      this.setState({ songData: data })
-      if (this.state.started) {
-        setTimeout(data => {
-          androidMetadata(data)
-
-          const newArt = `https://squid-radio.net/covers/${data.album}.jpg`
-
-          $('.glitch').addClass('glitch_img')
-          $('.glitch_sec').attr('src', this.state.currentArt)
-          $('.glitch_primary').attr('src', newArt)
-          setTimeout(() => {
-            $('.glitch').removeClass('glitch_img')
-            this.setState({ currentArt: newArt })
-          }, 1.5 * 1000)
-        }, this.streamDelay, data)
-      }
+    this.audio = new Audio(`https://play.squid-radio.net/${this.state.station}?cache_ts=${new Date().getTime()}`)
+    this.audio.onpause = () => {
+      // $('#playPauseIcon').html('play_arrow')
+      this.setState({ playing: false })
+      this.spin.pause()
+      anime({
+        targets: '#record',
+        rotate: '180deg',
+        duration: 2000,
+        easing: 'easeOutBack',
+        autoplay: true
+      })
     }
-  }
-
-  streamDelay = 0
-
-  componentDidMount () {
-    this.startSocket()
-    const spin = anime({
-      targets: '#record',
-      rotate: '1turn',
-      loop: true,
-      duration: 1100,
-      easing: 'linear',
-      autoplay: false
-    })
-
-    var audio = document.getElementById('audio-player')
-
-    $('.muted').click(function () {
-      audio.muted = !audio.muted
-      return false
-    })
-
-    // VOLUME BAR
-    // volume bar event
-    var volumeDrag = false
-    $('.volume').on('mousedown', function (e) {
-      volumeDrag = true
-      audio.muted = false
-      $('.sound').removeClass('muted')
-      updateVolume(e.pageX)
-    })
-    $(document).on('mouseup', function (e) {
-      if (volumeDrag) {
-        volumeDrag = false
-        updateVolume(e.pageX)
-      }
-    })
-    $(document).on('mousemove', function (e) {
-      if (volumeDrag) {
-        updateVolume(e.pageX)
-      }
-    })
-    var updateVolume = function (x, vol) {
-      var volume = $('.volume')
-      var percentage
-      // if only volume have specificed
-      // then direct update volume
-      if (vol) {
-        percentage = vol * 100
-      } else {
-        var position = x - volume.offset().left
-        percentage = 100 * position / volume.width()
-      }
-
-      if (percentage > 100) {
-        percentage = 100
-      }
-      if (percentage < 0) {
-        percentage = 0
-      }
-
-      // update volume bar and video volume
-      $('.volumeBar').css('width', percentage + '%')
-      audio.volume = percentage / 100
-
-      // change sound icon based on volume
-      if (audio.volume === 0) {
-        $('.sound').removeClass('sound2').addClass('muted')
-      } else if (audio.volume > 0.5) {
-        $('.sound').removeClass('muted').addClass('sound2')
-      } else {
-        $('.sound').removeClass('muted').removeClass('sound2')
-      }
-    }
-
-    // var volSlider = document.getElementById('volbar')
-
-    audio.onpause = function () {
-      $('#playPauseIcon').html('play_arrow')
-      spin.pause()
-      // stopRecord()
-    }
-    audio.onplay = function () {
+    this.audio.onplay = () => {
       if (!this.state.started) {
         // startSpectrum()
         let currentArt = `images/logo/soc_${this.state.station}.png`
@@ -193,23 +107,106 @@ export default class Radio extends React.Component {
         }, 2000)
         console.log('delay = ' + this.streamDelay)
       }
-      $('#playPauseIcon').html('pause')
-      spin.play()
-    }.bind(this)
+      // $('#playPauseIcon').html('pause')
+      this.setState({ playing: true })
+      this.spin.play()
+    }
+    this.audio.loop = false
+    this.audio.play()
 
-    const vdo = document.getElementById('cardFABPlay')
-    vdo.addEventListener('click', () => {
-      if (audio.paused) {
-        $('.waves-animation-one, #pause-button, .seek-field, .volume-icon, .volume-field, .info-two').show()
-        $('.waves-animation-two').hide()
-        $('#audio-player')[0].play()
-      } else {
-        $('.waves-animation-one, #pause-button, .seek-field, .volume-icon, .volume-field, .info-two').hide()
-        $('.waves-animation-two').show()
+    if (cb) cb()
+  }
 
-        audio.pause()
+  handlePlay = () => {
+    if (this.state.playing) this.audio.pause()
+    else this.audio.play()
+  }
+
+  handleSong = data => {
+    console.log(data)
+    if (data !== null) {
+      this.setState({ songData: data })
+      if (this.state.started) {
+        setTimeout(data => {
+          androidMetadata(data)
+
+          const newArt = `https://squid-radio.net/covers/${data.album}.jpg`
+
+          $('.glitch').addClass('glitch_img')
+          $('.glitch_sec').attr('src', this.state.currentArt)
+          $('.glitch_primary').attr('src', newArt)
+          setTimeout(() => {
+            $('.glitch').removeClass('glitch_img')
+            this.setState({ currentArt: newArt })
+          }, 1.5 * 1000)
+        }, this.streamDelay, data)
       }
-    }, false)
+    }
+  }
+
+  streamDelay = 0
+
+  componentDidMount () {
+    this.spin = anime({
+      targets: '#record',
+      rotate: '1turn',
+      loop: true,
+      duration: 1100,
+      easing: 'linear',
+      autoplay: false
+    })
+    this.startSocket()
+
+    // var audio = document.getElementById('audio-player')
+
+    $('.muted').click(function () {
+      this.audio.muted = !this.audio.muted
+      return false
+    })
+
+    // VOLUME BAR
+    // volume bar event
+    var volumeDrag = false
+    $('.volume').on('mousedown', (e) => {
+      volumeDrag = true
+      this.audio.muted = false
+      updateVolume(e.pageX)
+    })
+    $(document).on('mouseup', (e) => {
+      if (volumeDrag) {
+        volumeDrag = false
+        updateVolume(e.pageX)
+      }
+    })
+    $(document).on('mousemove', (e) => {
+      if (volumeDrag) {
+        updateVolume(e.pageX)
+      }
+    })
+    var updateVolume = (x, vol) => {
+      var volume = $('.volume')
+      var percentage
+      // if only volume have specificed
+      // then direct update volume
+      if (vol) {
+        percentage = vol * 100
+      } else {
+        var position = x - volume.offset().left
+        percentage = 100 * position / volume.width()
+      }
+
+      if (percentage > 100) {
+        percentage = 100
+      }
+      if (percentage < 0) {
+        percentage = 0
+      }
+
+      // update volume bar and video volume
+      $('.volumeBar').css('width', percentage + '%')
+      this.audio.volume = percentage / 100
+      this.setState({ muted: percentage === 0 })
+    }
   }
 
   render () {
@@ -221,13 +218,6 @@ export default class Radio extends React.Component {
             <div className='col s12 m10 offset-m1' id='TooHotCard'>
               <div className='card z-depth-5' style={{ borderRadius: '20px' }} id='TooHotPlayer'>
                 <div className='card-image' id='playerCardImage'>
-                  <video
-                    id='audio-player' className='responsive-video' autoPlay crossOrigin='anonymous' preload='auto'
-                    samesite='None' secure='true'
-                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }}
-                  >
-                    <source src={`https://play.squid-radio.net/${this.state.station}?cache_ts=${new Date().getTime()}`} type='audio/mpeg' />
-                  </video>
                   <div className='col s10 m8 l6' id='artDiv' style={{ paddingLeft: 0 }}>
                     <Record station={this.state.station} />
                     <div
@@ -243,16 +233,16 @@ export default class Radio extends React.Component {
 
                     </div>
                   </div>
-                  <a
-                    href='#'
+                  <div
                     id='cardFABPlay'
                     className='btn-floating z-depth-5 btn-large halfway-fab waves-effect waves-light red card-button'
-                    style={{ zIndex: 2 }} accessKey='p'
+                    style={{ zIndex: 2 }}
                   >
-                    <i className='material-icons card-icon' id='playPauseIcon'>
-                        play_arrow
+                    <i className='card-icon' id='playPauseIcon'>
+                      {this.state.muted ? <MdVolumeOff onClick={this.handlePlay} />
+                        : this.state.playing ? <MdPause onClick={this.handlePlay} /> : <MdPlayArrow onClick={this.handlePlay} />}
                     </i>
-                  </a>
+                  </div>
                 </div>
                 <div className='card-content black-text z-depth-5 left-align' id='playerControls'>
                   <canvas id='canvas' className='equaliser' />
