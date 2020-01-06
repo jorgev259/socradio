77a65c83c7e186708360d85c4acc8041cfe26364
jorgev259 <jorgev259@outlook.es>
2020-01-06 09:15:21 -0600

    background rework

--- src/js/background.jsx
+++ src/js/background.jsx
@@ -3,6 +3,32 @@ import info from './bg.json'
 import Config from './Config'
 import anime from 'animejs/lib/anime.es.js'
 
+class ItemBG extends React.Component {
+  render () {
+    const bg = info[this.props.station][this.props.index] || (this.props.index < 0 ? info[this.props.station][info[this.props.station].length - 1] : info[this.props.station][0])
+    const type = bg.endsWith('.mp4') ? 'video' : 'image'
+    switch (type) {
+      case 'video':
+        return (
+          <video className='myBG' autoPlay muted loop id={this.props.id}>
+            <source src={bg} type='video/mp4' />
+          </video>
+        )
+
+      default:
+        return (
+          <div
+            className='myBG'
+            id={this.props.id}
+            style={{
+              backgroundImage: `url("${bg}")`
+            }}
+          />
+        )
+    }
+  }
+}
+
 export default class Background extends React.Component {
   spin = {}
   state = {
@@ -12,65 +38,11 @@ export default class Background extends React.Component {
 
   updateBG = () => {
     if (this.state.anim === 'running') return
-    let index = this.state.bgIndex
-    if (index + 1 >= info[this.props.station].length) index = 0
-    else index++
 
     this.spin.play()
-    this.setState({ anim: 'running', bgIndex: index }, () => {
-      anime({
-        targets: '#newBG',
-        duration: 1100,
-        translateX: '100%',
-        easing: 'spring(1, 80, 10, 0)',
-        complete: (anim) => {
-          this.setState({ anim: 'stale' })
-        }
-      })
+    this.setState({ anim: 'running' }, () => {
+      this.bgdrag.play()
     })
-  }
-
-  getType (id, index, z) {
-    const bg = info[this.props.station][index] || (index < 0 ? info[this.props.station][info[this.props.station].length - 1] : info[this.props.station][0])
-    const type = bg.endsWith('.mp4') ? 'video' : 'image'
-    switch (type) {
-      case 'video':
-        return (
-          <video className='myBG' autoPlay muted loop id={id} style={{ zIndex: z }}>
-            <source src={bg} type='video/mp4' />
-          </video>
-        )
-
-      default:
-        return (
-          <div
-            className='myBG'
-            id={id}
-            style={{
-              zIndex: z,
-              backgroundImage: `url("${bg}")`
-            }}
-          />
-        )
-    }
-  }
-
-  getAnim () {
-    switch (this.state.anim) {
-      case 'stale':
-        return this.getType('myBG', this.state.bgIndex, 1)
-
-      case 'running': {
-        const newBG = this.getType('newBG', this.state.bgIndex, 2)
-        const oldBG = this.getType('oldBG', this.state.bgIndex - 1, 1)
-        return (
-          <>
-            {oldBG}
-            {newBG}
-          </>
-        )
-      }
-    }
   }
 
   componentDidMount () {
@@ -81,13 +53,30 @@ export default class Background extends React.Component {
       easing: 'spring(1, 50, 10, 0)',
       autoplay: false
     })
+
+    this.bgdrag = anime({
+      targets: '#newBG',
+      duration: 1100,
+      translateX: '100%',
+      easing: 'spring(1, 80, 10, 0)',
+      autoplay: false,
+      complete: (anim) => {
+        let index = this.state.bgIndex
+        if (index + 1 >= info[this.props.station].length) index = 0
+        else index++
+        this.setState({ anim: 'stale', bgIndex: index }, () => {
+          anim.reset()
+        })
+      }
+    })
   }
 
   render () {
     return (
       <>
         <Config station={this.props.station} handleBG={this.updateBG.bind(this)} handleStation={this.props.updateStation} />
-        {this.getAnim()}
+        <ItemBG id='currentBG' station={this.props.station} index={this.state.bgIndex} />
+        <ItemBG id='newBG' station={this.props.station} index={this.state.bgIndex + 1} />
       </>
     )
   }
