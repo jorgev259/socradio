65adaa09638a98fcd0410c4e8d7e47ba8e1325fd
jorgev259 <jorgev259@outlook.es>
2020-06-13 20:48:08 -0600

    asd

--- src/Radio.js
+++ src/Radio.js
@@ -13,8 +13,8 @@ import './css/font.css'
 import anime from 'animejs/lib/anime.es.js'
 
 import 'dat.gui'
-import Background from './js/Background'
-import Record from './js/Record'
+import Background from './js/Background.jsx'
+import Record from './js/Record.jsx'
 
 function androidMetadata (data) {
   if ('mediaSession' in navigator) {
