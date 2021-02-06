7d15d13342cb14539acf6d8babd32350e8fb0fae
jorgev259 <jorgev259@outlook.es>
2021-02-05 23:03:03 -0600

    .

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { useRoutes } from 'hookrouter'
 import Radio from './Radio'
 import Small from './Small'
 import Widget from './Widget'
+import Board from './board'
 
 import './css/global.css'
 
 const routes = {
   '/': () => <Radio station='clouds' />,
   '/small': () => <Small station='clouds' />,
+  '/board': () => <Board station='clouds' />,
   '/small/:station': ({ station }) => <Small station={station} />,
   '/widget': () => <Widget />,
   '/:station': ({ station }) => <Radio station={station} />
